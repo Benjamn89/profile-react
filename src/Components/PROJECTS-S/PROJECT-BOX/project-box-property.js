f3082ee349bc7e123c5f04ee105921e0c3cb895b
unknown <newbennytal@gmail.com>
2020-03-29 02:29:01 +0300

    update the project link

--- src/Components/PROJECTS-S/PROJECT-BOX/project-box-property.js
+++ src/Components/PROJECTS-S/PROJECT-BOX/project-box-property.js
@@ -17,7 +17,7 @@ var ProjectsBoxProperty = [
     objects: {
       projectSum: "My Profile",
       usedSkills: "React",
-      linkTo: "http://localhost:3000/",
+      linkTo: "https://priceless-shannon-1714e7.netlify.com/",
       img: ProfileImg,
       alt: "profile"
     }
