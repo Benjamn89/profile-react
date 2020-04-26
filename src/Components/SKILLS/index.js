34e1fc5f54d004f5f82e30cf422d3ef353f4655e
unknown <newbennytal@gmail.com>
2020-04-26 03:38:35 +0300

    remove font awosome out of my face!

--- src/Components/SKILLS/index.js
+++ src/Components/SKILLS/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
+import SkillList from "./skill-list";
+import "./index.css";
+// Import media
 import SqlPNG from "../../media/SQL.png";
 import Csharp from "../../media/Csharp.png";
 import Cplus from "../../media/Cplus.png";
-import SkillList from "./skill-list";
-import "./index.css";
+import Html5 from "../../media/html5.png";
+import Css from "../../media/css.png";
+import Bootstrap from "../../media/bootstrap.png";
+import Javascript from "../../media/javascript.png";
+import Nodejs from "../../media/nodejs.png";
+import Reactjs from "../../media/react.png";
+import Wordpress from "../../media/wordpress.png";
 
 class Skills extends Component {
   shouldComponentUpdate() {
@@ -19,20 +27,20 @@ class Skills extends Component {
           PROGRAMMING LANGUAGES & TOOLS
         </p>
         <div className="skills-icon-div">
-          <i className="fab fa-html5 fa-4x"></i>
-          <i className="fab fa-css3-alt fa-4x"></i>
-          <i className="fab fa-js-square fa-4x"></i>
-          <i className="fab fa-bootstrap fa-4x"></i>
-          <i className="fab fa-node-js fa-4x"></i>
-          <i className="fab fa-react fa-4x"></i>
-          <i className="fab fa-wordpress fa-4x"></i>
+          <img src={Html5} alt="html5" />
+          <img src={Css} alt="css" />
+          <img src={Javascript} alt="javascript" />
+          <img src={Bootstrap} alt="bootstrap" />
+          <img src={Reactjs} alt="react" />
+          <img src={Nodejs} alt="nodejs" />
+          <img src={Wordpress} alt="wordpress" />
           <img alt="sqlpng" src={SqlPNG} />
           <img alt="cpng" src={Csharp} />
           <img alt="cplus" src={Cplus} />
         </div>
         <p className="schools-skills-p add-skills-p">ADDITIONAL SKILLS</p>
         <ul className="add-skills-ul">
-          {SkillList.map(el => {
+          {SkillList.map((el) => {
             return (
               <li key={el}>
                 <i className="fas fa-check"></i>
