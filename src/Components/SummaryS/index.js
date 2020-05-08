484515968d0f43ffcf513f3f90d939ca90f0062a
unknown <newbennytal@gmail.com>
2020-05-08 11:46:38 +0300

    spelling

--- src/Components/SummaryS/index.js
+++ src/Components/SummaryS/index.js
@@ -24,15 +24,15 @@ class SummaryS extends Component {
             and passionate about making things happen with.
             <br />
             But, I never took a step for making it my profession.
-            <br /> <br />2 years ago i decided to HUNT after what i want to
+            <br /> <br />2 years ago i decided to HUNT after what I want to
             myself. <br />I started learning/ exploring/ deepening web
             development! And as i going on i realize that my passion growing
             more and more.
             <br /> <br />
-            Now i need you, to open the door for me for this fascinating world,
+            Now I need you, to open the door for me for this fascinating world,
             to give me the chance to prove myself and make my dream to reality.
             <br />
-            In return i promise that you will get my 100% every single day, you
+            In return I promise that you will get my 100% every single day, you
             will get an employee who does not give up to himself, who will push
             to understand, improve, give and optimize his efficiency at any
             given time.
