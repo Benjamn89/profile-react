32e75a51ca8e6181291e00ab820b655050087812
unknown <newbennytal@gmail.com>
2020-05-26 03:11:55 +0300

    projects updated

--- src/Components/PROJECTS-S/PROJECT-BOX/project-box-property.js
+++ src/Components/PROJECTS-S/PROJECT-BOX/project-box-property.js
@@ -1,10 +1,8 @@
 import MinyanImg from "../../../media/Minyan-project.PNG";
-import ProfileImg from "../../../media/Profile-project.PNG";
-import HamburgerImg from "../../../media/Hamburger.PNG";
-import MyShop from "../../../media/shop-react2.PNG";
 import Allalouf from "../../../media/Allalouf.PNG";
 import Sted from "../../../media/sted.png";
 import Medallie from "../../../media/medallie.png";
+import ToyShop from "../../../media/toy-shop.png";
 
 var ProjectsBoxProperty = [
   {
@@ -23,9 +21,20 @@ var ProjectsBoxProperty = [
         "FaundaDB - NoSQL database with real time data",
         "Firebase Storage bucket for loading media",
         "Full communicate Front end - Backend",
-        "Authentication via database",
+        "Authentication with DB",
         "Local Storage",
       ],
+    },
+  },
+  {
+    objects: {
+      projectSum: "Online ToyShop",
+      usedSkills: "React/ Redux/ SASS",
+      linkTo: "https://toy-s.netlify.app/",
+      img: ToyShop,
+      alt: "Toys-shop",
+      medallie: Medallie,
+      tools: ["Project will be completed and updated in the comming 2/3 days"],
     },
   },
   {
@@ -46,26 +55,6 @@ var ProjectsBoxProperty = [
   },
   {
     objects: {
-      projectSum: "Shopping Online",
-      usedSkills: "React",
-      linkTo: "https://shoping-with-react.netlify.com/",
-      img: MyShop,
-      alt: "Shopping",
-      tools: ["Create React App", "Redux"],
-    },
-  },
-  {
-    objects: {
-      projectSum: "Hamburger",
-      usedSkills: "React",
-      linkTo: "https://hamburger-binyamin.netlify.com/",
-      img: HamburgerImg,
-      alt: "Hamburger",
-      tools: ["Create React App", "Redux"],
-    },
-  },
-  {
-    objects: {
       projectSum: "Webapp that finds prayers",
       usedSkills: "NodeJS, MongoDB",
       linkTo: "https://tal-minyan.herokuapp.com/",
@@ -77,18 +66,6 @@ var ProjectsBoxProperty = [
         "MongoDB",
         "Sendgrid - For sending Emails",
         "Select2",
-      ],
-    },
-  },
-  {
-    objects: {
-      projectSum: "My Profile",
-      usedSkills: "React",
-      linkTo: "https://binyamintal.netlify.com/",
-      img: ProfileImg,
-      alt: "profile",
-      tools: [
-        "Create React App (for practicing React syntax/ reuse of React components",
       ],
     },
   },
